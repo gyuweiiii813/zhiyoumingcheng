6694761447d5469820db40e635ceaf2746a46a73
gyuweiiii813 <gyuweiiii@gmail.com>
2026-06-23 22:37:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -13455,7 +13455,7 @@ setTimeout(removeDuplicateAttractionsByName, 6000);
         let progress = 0;
 
         
-        const speed = 2;
+        const speed = 3;
 
         const movingStyle = createMovingLocationIcon();
 
