d1a9ef8a2f58e63c86047d6aae698c5b95171809
gyuweiiii813 <gyuweiiii@gmail.com>
2026-06-23 21:48:11 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2180,7 +2180,7 @@ function playTrackAnimation(points) {
     
     let currentIndex = 0;
     let progress = 0;
-    const speed = 2;    
+    const speed = 2.5;    
     function animate() {
         if (currentIndex >= points.length - 1) {
             return;
