56743e6b81350d1db69ec9fd6d553f263e5286dc
gyuweiiii813 <gyuweiiii@gmail.com>
2026-06-24 15:57:23 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -12042,6 +12042,7 @@ setTimeout(removeDuplicateAttractionsByName, 6000);
 
         trafficStepLayer = new ol.layer.Vector({
             source: trafficStepSource,
+            visible: false,
             zIndex: 2600,
             style: function (feature) {
                 const index = feature.get('index') || '';
